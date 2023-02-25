fac8679e3c0d715f352feaa3a90dc1bd48098c6b
sarahliz24 <sarahliz24@hotmail.com>
2023-02-25 11:33:42 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,30 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game "loop", called when the scrdpit is first loadeed
  * and after the uses's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     //creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}.  Aborting!`;
+    }  
 }
 
 function checkAnwer(params) {
@@ -40,8 +49,10 @@ function incrementWrongAnswer(params) {
     
 }
 
-function displayAdditionQuestion(params) {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(params) {
