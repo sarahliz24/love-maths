ddaa01bf06f03ebcfa2e84b721b9f281abc8ab46
sarahliz24 <sarahliz24@hotmail.com>
2023-02-25 10:51:16 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame()
+{
+
+}
+
+function checkAnwer(params) {
+    
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
+
+function displayDivideQuestione(params) {
+    
+}
